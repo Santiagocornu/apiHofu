e8ed13dce8721954ecfc06a3702e4743380d00de
santiagocornu <santiagocornue.p.e.t20@gmail.com>
2025-01-05 07:31:20 -0300

    añadiendo homeController

---
+++ java/src/main/java/com/proyectoMama/proyectoMama/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.proyectoMama.proyectoMama.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Bienvenido a la API de Hofu Sushi");
+    }
+}
+
