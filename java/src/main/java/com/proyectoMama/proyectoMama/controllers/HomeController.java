53cdb7e2c340237047b49ce0a75d7339f487cbb3
santiagocornu <santiagocornue.p.e.t20@gmail.com>
2025-01-05 07:32:55 -0300

    añadiendo homeController

--- java/src/main/java/com/proyectoMama/proyectoMama/controllers/HomeController.java
+++ java/src/main/java/com/proyectoMama/proyectoMama/controllers/HomeController.java
@@ -1,12 +1,15 @@
 package com.proyectoMama.proyectoMama.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@CrossOrigin(origins = {"http://localhost:3000", "https://front-hofu.vercel.app"})
+
 public class HomeController {
 
     @GetMapping
