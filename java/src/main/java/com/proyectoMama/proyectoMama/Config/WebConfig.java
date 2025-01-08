4263c39d643594db6f13c8fcf9f9a888503255cc
santiagocornu <santiagocornue.p.e.t20@gmail.com>
2025-01-08 10:37:57 -0300

    cambiando meu crosorigin

--- java/src/main/java/com/proyectoMama/proyectoMama/Config/WebConfig.java
+++ java/src/main/java/com/proyectoMama/proyectoMama/Config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "https://front-hofu.vercel.app")
+                .allowedOrigins("http://localhost:3000", "https://front-hofu.vercel.app", "https://front-hofu-e89pbj60m-santiagocornus-projects.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
